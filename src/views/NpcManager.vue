d9d24f4a4ea166a246771d6bfe8adb12515679fd
V-sion-V <1796469419@qq.com>
2024-06-30 12:54:39 +0800

    O.1.4 npc manager without add and save

--- src/views/NpcManager.vue
+++ src/views/NpcManager.vue
@@ -5,7 +5,21 @@ import axios from 'axios'
 import router from '@/router/index.js'
 import { useThemeVars } from 'naive-ui'
 
-const themeVars = useThemeVars()
+function animate(duration, timing, update, complete) {
+  let start = performance.now()
+  requestAnimationFrame(function animate(time) {
+    let timeFraction = (time - start) / duration
+    if (timeFraction > 1) timeFraction = 1
+
+    update(timing(timeFraction))
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate)
+    } else {
+      complete()
+    }
+  })
+}
 
 const npcListContext = ref({
   loading: true,
@@ -24,11 +38,19 @@ const npcListContext = ref({
 
 const npcInfoContext = ref({
   loading: false,
+  isEditMode: false,
   currentFamiliarLevel: -1,
+  leftPanelHidePercentage: 0,
+  savedCurrentNpcInfo: {
+    id: null,
+    npcID: null,
+    name: null,
+    familiarList: []
+  },
   currentNpcInfo: {
-    id: '00008',
-    npcID: 'TestNPC_8',
-    name: 'V_sion8',
+    id: null,
+    npcID: null,
+    name: null,
     familiarList: []
   }
 })
@@ -37,9 +59,11 @@ loadNpcList()
 
 function loadNpcList() {
   npcListContext.value.loading = true
+  npcListContext.value.currentNpcIndex = -1
   axios.post('/npc/getList').then((res) => {
     npcListContext.value.loading = false
     npcListContext.value.npcList = res.data.npcList
+    npcListContext.value.npcList.sort((a, b) => a.npcID.localeCompare(b.npcID))
   }).catch((e) => {
     console.log(e)
   })
@@ -52,12 +76,11 @@ function changeCurrentNpc(index) {
   npcListContext.value.currentNpcIndex = index
   npcInfoContext.value.loading = true
   npcInfoContext.value.currentFamiliarLevel = -1
+  npcInfoContext.value.isEditMode = false
   axios.post('/npc/getInfo', { id: npcListContext.value.npcList[index].id }).then((res) => {
     npcInfoContext.value.loading = false
     npcInfoContext.value.currentNpcInfo = res.data
-    for (let i = 0; i < npcInfoContext.value.currentNpcInfo.familiarList.length; i++) {
-      npcInfoContext.value.currentNpcInfo.familiarList[i].familiarLevel = i
-    }
+    npcInfoContext.value.savedCurrentNpcInfo = JSON.parse(JSON.stringify(npcInfoContext.value.currentNpcInfo))
   }).catch((e) => {
     console.log(e)
   })
@@ -71,6 +94,69 @@ const familiarLevelInfo = computed(() => {
   return npcInfoContext.value.currentNpcInfo.familiarList[npcInfoContext.value.currentFamiliarLevel]
 })
 
+function enterEditMode() {
+  npcInfoContext.value.isEditMode = true
+  animate(
+    250,
+    (x)=>1-Math.pow(1-x, 3),
+    (x)=>npcInfoContext.value.leftPanelHidePercentage = x,
+    ()=>{}
+  )
+}
+
+function addLocalFamiliarLevel() {
+  npcInfoContext.value.currentNpcInfo.familiarList.push({
+    id:'',
+    maxSubFamiliarLevel:1,
+    dialogCount:0
+  })
+}
+
+function deleteLocalFamiliarLevel(level) {
+  npcInfoContext.value.currentNpcInfo.familiarList.splice(level, 1)
+  if(level >= npcInfoContext.value.currentNpcInfo.familiarList.length)
+    npcInfoContext.value.currentFamiliarLevel = level - 1
+}
+
+function exitEditMode() {
+  npcInfoContext.value.isEditMode = false
+  npcInfoContext.value.currentNpcInfo = JSON.parse(JSON.stringify(npcInfoContext.value.savedCurrentNpcInfo))
+  animate(
+    250,
+    (x)=>1-Math.pow(1-x, 3),
+    (x)=>npcInfoContext.value.leftPanelHidePercentage = 1-x,
+    ()=>{}
+  )
+}
+
+const isDirty = computed(()=>{
+  let current = npcInfoContext.value.currentNpcInfo
+  let origin = npcInfoContext.value.savedCurrentNpcInfo
+
+  if(origin.npcID !== current.npcID) return true;
+  if(origin.name !== current.name) return true;
+  if(origin.id !== current.id) return true;
+  if(origin.familiarList.length !== current.familiarList.length) return true;
+  for(let i = 0; i < origin.familiarList.length; i++) {
+    if(origin.familiarList[i].maxSubFamiliarLevel !== current.familiarList[i].maxSubFamiliarLevel) return true;
+    if(origin.familiarList[i].id !== current.familiarList[i].id) return true;
+  }
+  return false
+})
+
+function applyCurrentChanges() {
+
+}
+
+function applyDeleteCurrentNpc() {
+  let currentID = npcInfoContext.value.currentNpcInfo.id
+  axios.post('/npc/delete', { id: currentID }).then((res) => {
+    loadNpcList()
+  }).catch((e) => {
+    console.log(e)
+  })
+}
+
 </script>
 
 <template>
@@ -78,14 +164,15 @@ const familiarLevelInfo = computed(() => {
     <n-spin v-if="npcListContext.loading"
             style="width: 100%; height: 100%"
             size="large" />
-    <n-flex v-else justify="left" style="height: 100%;max-height:100%;column-gap: 0">
-      <n-flex vertical style="max-height: 100%;width: 30%;gap:0;">
+    <n-flex v-else justify="left"
+            :style="'height: 100%;column-gap: 0;margin-left:'+(-npcInfoContext.leftPanelHidePercentage*30/0.7)+'%'">
+      <n-flex vertical style="max-height: 100%;width: 30% ;gap:0">
         <n-flex style="padding: 20px 20px">
           <n-h2 type="success" style="margin-bottom: 0;" prefix="bar">
             NPC List
           </n-h2>
-          <n-flex justify="right" style="flex:1">
-            <n-button circle tertiary type="info" style="" @click="tryAddNpc()">
+          <n-flex justify="right" style="gap:0;flex:1">
+            <n-button circle tertiary type="info" style="margin: auto 5px" @click="tryAddNpc()">
               <template #icon>
                 <n-icon>
                   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -98,7 +185,7 @@ const familiarLevelInfo = computed(() => {
                 </n-icon>
               </template>
             </n-button>
-            <n-button circle tertiary type="primary" style="" @click="router.push('/')">
+            <n-button circle tertiary type="primary" style="margin: auto 5px" @click="router.push('/')">
               <template #icon>
                 <n-icon>
                   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -154,19 +241,109 @@ const familiarLevelInfo = computed(() => {
                 style="width: 100%; height: 100%"
                 size="large" />
         <n-flex v-else vertical style="height: 100%;gap: 0">
-          <n-h2 type="default" style="margin: 20px 40px" prefix="bar" align-text>
-            NPC Information
-          </n-h2>
-          <n-flex justify="left" align="top" style="margin: 10px 40px 30px 40px">
-            <n-statistic label="Name" style="margin: 0 auto; width: 30%">
-              {{ npcInfoContext.currentNpcInfo.name }}
-            </n-statistic>
-            <n-statistic label="NPC ID" style="margin: 0 auto; width: 30%">
-              {{ npcInfoContext.currentNpcInfo.npcID }}
-            </n-statistic>
-            <n-statistic label="Database ID" style="margin: 0 auto; width: 30%">
-              {{ npcInfoContext.currentNpcInfo.id }}
-            </n-statistic>
+          <n-flex style="margin: 0; gap: 0">
+            <n-h2 type="default" style="margin: 20px 0 20px 40px" prefix="bar" align-text>
+              NPC Information
+            </n-h2>
+            <n-button v-if="!npcInfoContext.isEditMode"
+                      circle tertiary
+                      type="info" style="margin:auto 5px auto 15px"
+                      @click="enterEditMode()">
+              <template #icon>
+                <n-icon>
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                       viewBox="0 0 32 32">
+                    <path d="M2 26h28v2H2z" fill="currentColor"></path>
+                    <path
+                      d="M25.4 9c.8-.8.8-2 0-2.8l-3.6-3.6c-.8-.8-2-.8-2.8 0l-15 15V24h6.4l15-15zm-5-5L24 7.6l-3 3L17.4 7l3-3zM6 22v-3.6l10-10l3.6 3.6l-10 10H6z"
+                      fill="currentColor"></path>
+                  </svg>
+                </n-icon>
+              </template>
+            </n-button>
+            <n-popconfirm
+              v-if="!npcInfoContext.isEditMode"
+              @positive-click="applyDeleteCurrentNpc()"
+              placement="bottom"
+              :positive-button-props="{type:'error'}"
+              :show-icon="false"
+            >
+              <template #trigger>
+                <n-button circle type="error" tertiary style="margin: auto 5px">
+                  <template #icon>
+                    <n-icon>
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                           viewBox="0 0 32 32">
+                        <path d="M12 12h2v12h-2z" fill="currentColor"></path>
+                        <path d="M18 12h2v12h-2z" fill="currentColor"></path>
+                        <path d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20z"
+                              fill="currentColor"></path>
+                        <path d="M12 2h8v2h-8z" fill="currentColor"></path>
+                      </svg>
+                    </n-icon>
+                  </template>
+                </n-button>
+              </template>
+              <n-text style="text-align: center;margin:auto">
+                Delete NPC {{npcInfoContext.currentNpcInfo.name}}
+              </n-text>
+            </n-popconfirm>
+            <n-button v-if="npcInfoContext.isEditMode"
+                      circle tertiary
+                      :type="isDirty?'error':'info'" style="margin:auto 5px auto 15px"
+                      @click="exitEditMode()">
+              <template #icon>
+                <n-icon>
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                       viewBox="0 0 512 512">
+                    <path
+                      d="M289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 0 0 0-34 34l95 95l-95 95a24 24 0 1 0 34 34l95-95l95 95a24 24 0 0 0 34-34z"
+                      fill="currentColor"></path>
+                  </svg>
+                </n-icon>
+              </template>
+            </n-button>
+            <n-button v-if="npcInfoContext.isEditMode"
+                      circle :disabled="!isDirty" :type="isDirty?'success':'default'"
+                      style="margin: auto 5px"
+                      @click="applyCurrentChanges()">
+              <template #icon>
+                <n-icon>
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                       viewBox="0 0 32 32">
+                    <path d="M13 24l-9-9l1.414-1.414L13 21.171L26.586 7.586L28 9L13 24z" fill="currentColor"></path>
+                  </svg>
+                </n-icon>
+              </template>
+            </n-button>
+          </n-flex>
+          <n-flex justify="left" align="top" style="margin: 10px 40px 30px 40px; height: 90px">
+            <n-form-item label="Name" style="margin: 0 auto; width: 30%;"
+                         label-style="color:rgba(255, 255, 255, 0.52);">
+              <n-input v-if="npcInfoContext.isEditMode"
+                       v-model:value="npcInfoContext.currentNpcInfo.name"
+                       placeholder="" size="large"
+              />
+              <n-text v-else style="font-size: 24px">
+                {{ npcInfoContext.currentNpcInfo.name }}
+              </n-text>
+            </n-form-item>
+            <n-form-item label="NPC ID" style="margin: 0 auto; width: 30%;"
+                         label-style="color:rgba(255, 255, 255, 0.52);">
+              <n-input v-if="npcInfoContext.isEditMode"
+                       v-model:value="npcInfoContext.currentNpcInfo.npcID"
+                       placeholder="" size="large"
+              />
+              <n-text v-else style="font-size: 24px">
+                {{ npcInfoContext.currentNpcInfo.npcID }}
+              </n-text>
+            </n-form-item>
+            <n-form-item label="Database ID" style="margin: 0 auto; width: 30%;"
+                         label-style="color:rgba(255, 255, 255, 0.52);">
+              <n-text style="font-size: 24px">
+                {{ npcInfoContext.currentNpcInfo.id ? npcInfoContext.currentNpcInfo.id : 'NO INFO' }}
+              </n-text>
+            </n-form-item>
           </n-flex>
           <n-divider style="margin:0" />
           <n-flex style="flex: 1;gap: 0;overflow: auto">
@@ -177,15 +354,15 @@ const familiarLevelInfo = computed(() => {
               <n-divider style="margin: 0;" />
               <n-scrollbar style="flex:1;width: 100%;">
                 <n-flex style="width: 100%;padding:20px;" vertical>
-                  <n-card v-for="item in npcInfoContext.currentNpcInfo.familiarList"
-                          :class="item.familiarLevel===npcInfoContext.currentFamiliarLevel?'selected-n-card':'hovered-n-card'"
-                          hoverable @click="changeCurrentFamiliar(item.familiarLevel)"
+                  <n-card v-for="(item, index) in npcInfoContext.currentNpcInfo.familiarList"
+                          :class="index===npcInfoContext.currentFamiliarLevel?'selected-n-card':'hovered-n-card'"
+                          hoverable @click="changeCurrentFamiliar(index)"
                   >
                     <template #header>
-                      <n-h3 :type="item.familiarLevel===npcInfoContext.currentFamiliarLevel?'success':'info'"
+                      <n-h3 :type="index===npcInfoContext.currentFamiliarLevel?'success':'info'"
                             style="margin-bottom: 0;width: 100%" align-text prefix="bar">
                         <n-text>
-                          Level. {{ item.familiarLevel }}
+                          Level. {{ index }}
                         </n-text>
                       </n-h3>
                     </template>
@@ -195,6 +372,22 @@ const familiarLevelInfo = computed(() => {
                       </n-text>
                     </template>
                   </n-card>
+                  <n-button v-if="npcInfoContext.isEditMode"
+                            style="border-radius: 7px;" dashed
+                            @click="addLocalFamiliarLevel">
+                    <template #icon>
+                      <n-icon style="margin:auto 0;" type="info">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             viewBox="0 0 28 28">
+                          <g fill="none">
+                            <path d="M24 13h-9V4a1 1 0 1 0-2 0v9H4a1 1 0 1 0 0 2h9v9a1 1 0 1 0 2 0v-9h9a1 1 0 1 0 0-2z"
+                                  fill="currentColor"></path>
+                          </g>
+                        </svg>
+                      </n-icon>
+                    </template>
+                    Add Familiar Level
+                  </n-button>
                 </n-flex>
               </n-scrollbar>
             </n-flex>
@@ -207,16 +400,43 @@ const familiarLevelInfo = computed(() => {
             >
             </n-empty>
             <n-flex v-else vertical style="flex: 1;gap:0;">
-              <n-h3 type="default" prefix="bar" style="margin: 20px 35px;" align-text>
-                Familiar Level {{ npcInfoContext.currentFamiliarLevel }}
-              </n-h3>
+              <n-flex style="gap: 0;">
+                <n-h3 type="default" prefix="bar" style="margin:20px 0 20px 35px; width: 140px" align-text>
+                  Familiar Level {{ npcInfoContext.currentFamiliarLevel }}
+                </n-h3>
+                <n-button v-if="npcInfoContext.isEditMode"
+                          circle type="error" tertiary style="margin: auto 5px"
+                          @click="deleteLocalFamiliarLevel(npcInfoContext.currentFamiliarLevel)">
+                  <template #icon>
+                    <n-icon>
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                           viewBox="0 0 32 32">
+                        <path d="M12 12h2v12h-2z" fill="currentColor"></path>
+                        <path d="M18 12h2v12h-2z" fill="currentColor"></path>
+                        <path d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20z"
+                              fill="currentColor"></path>
+                        <path d="M12 2h8v2h-8z" fill="currentColor"></path>
+                      </svg>
+                    </n-icon>
+                  </template>
+                </n-button>
+              </n-flex>
               <n-flex justify="left" align="top" style="margin: 8px 40px 20px 40px">
-                <n-statistic label="Max Sub-Familiar-Level" style="margin: 0 auto; width: 40%">
-                  {{ familiarLevelInfo.maxSubFamiliarLevel }}
-                </n-statistic>
-                <n-statistic label="Database ID" style="margin: 0 auto; width: 40%">
-                  {{ familiarLevelInfo.id }}
-                </n-statistic>
+                <n-form-item label="Max SubLevel" style="margin: 0 auto; width: 40%;"
+                             label-style="color:rgba(255, 255, 255, 0.52);">
+                  <n-input-number v-if="npcInfoContext.isEditMode"
+                                  v-model:value="familiarLevelInfo.maxSubFamiliarLevel"
+                                  size="large" placeholder="最小值" :min="1" />
+                  <n-text v-else style="font-size: 24px">
+                    {{ familiarLevelInfo.maxSubFamiliarLevel }}
+                  </n-text>
+                </n-form-item>
+                <n-form-item label="Database ID" style="margin: 0 auto; width: 40%;"
+                             label-style="color:rgba(255, 255, 255, 0.52);">
+                  <n-text style="font-size: 24px">
+                    {{ familiarLevelInfo.id ? familiarLevelInfo.id : 'NO INFO' }}
+                  </n-text>
+                </n-form-item>
               </n-flex>
               <n-card hoverable
                       size="medium"
@@ -224,7 +444,7 @@ const familiarLevelInfo = computed(() => {
               >
                 <template #header>
                   <n-flex>
-                    <n-icon size="30">
+                    <n-icon size="30" color="#63e2b7">
                       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                            viewBox="0 0 24 24">
                         <g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -243,6 +463,7 @@ const familiarLevelInfo = computed(() => {
                 </template>
                 <template #header-extra>
                   <n-button round tertiary icon-placement="right"
+                            :disabled="npcInfoContext.isEditMode"
                             @click="router.push(`dialog/${familiarLevelInfo.id}`)"
                   >
                     Open dialog editor
@@ -260,7 +481,7 @@ const familiarLevelInfo = computed(() => {
                     </template>
                   </n-button>
                 </template>
-                <n-divider style="margin-top:-2px;margin-bottom: 12px"/>
+                <n-divider style="margin-top:-2px;margin-bottom: 12px" />
                 <n-flex>
                   <n-statistic label="Dialog Count" style="margin: 0; width: 40%">
                     {{ familiarLevelInfo.dialogCount }}
