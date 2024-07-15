f510f752ab9a3db38bad30fe5b87e6c9b9448f21
V-sion-V <1796469419@qq.com>
2024-07-16 01:40:19 +0800

    hash login test

--- src/main.js
+++ src/main.js
@@ -11,10 +11,15 @@ app.use(router)
 import naive from 'naive-ui'
 app.use(naive)
 
+//cookie
+import VueCookies from 'vue-cookies'
+app.use(VueCookies, { expires: '7d'})
+
 //Vue flow
 import "@vue-flow/core/dist/style.css"
 import "@vue-flow/core/dist/theme-default.css"
 
+//Axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://172.16.3.72:5000/';
 axios.defaults.withCredentials = true;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,34 @@
 <script setup>
-import { VueFlow } from '@vue-flow/core'
-import { ref } from 'vue'
+import sha256 from 'crypto-js/sha256.js'
+import { computed, inject, ref } from 'vue'
+const $cookies = inject('$cookies');
 
+const userName = ref('')
+const password = ref('')
+function tryLogin() {
+  let passwordHash = sha256(password.value).toString()
+
+}
+
+const isLoginValid = computed(()=>{
+  return userName.value.length >= 6 && password.value.length >= 6
+})
 </script>
 
 <template>
   <n-flex vertical style="height: 100%; width: 100%">
     <n-card style="margin: auto; width: 500px;padding: 20px">
       <n-flex vertical>
-        <n-h1 prefix="bar"> Spac Toolchain </n-h1>
+        <n-h1 prefix="bar"> Login Spac Toolchain </n-h1>
         <n-form>
           <n-form-item label="Username">
-            <n-input></n-input>
+            <n-input v-model:value="userName"></n-input>
           </n-form-item>
           <n-form-item label="Password">
-            <n-input></n-input>
+            <n-input v-model:value="password"></n-input>
           </n-form-item>
         </n-form>
-        <n-button tertiary type="success">
+        <n-button :type="isLoginValid?'success':'default'" :disabled="!isLoginValid" @click="tryLogin">
           Login
         </n-button>
       </n-flex>
